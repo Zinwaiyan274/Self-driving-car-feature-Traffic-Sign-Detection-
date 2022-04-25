0a1440681e9ebde99ac16dfa7e74a230b68d5c4c
Waii <76773473+Zinwaiyan274@users.noreply.github.com>
2022-04-25 16:39:55 +0630

    Update main.py

--- main.py
+++ main.py
@@ -37,57 +37,57 @@ import streamlit as st
 def cal():
     import streamlit as st
 
-    st.title('Traffic sign')
-    st.subheader("from the mind of Team ZAC.")
+#     st.title('Traffic sign')
+#     st.subheader("from the mind of Team ZAC.")
 
-    weight = st.number_input("Enter your weight(in Kgs)")
+#     weight = st.number_input("Enter your weight(in Kgs)")
 
-    status = st.radio('Select your height format: ', ('cms', 'meters', 'feet'))
+#     status = st.radio('Select your height format: ', ('cms', 'meters', 'feet'))
 
-    if status == 'cms':
-        height = st.number_input('Centimeters')
-        try:
-            bmi = weight / ((height / 100) ** 2)
-        except:
-            st.text("Enter some value of height")
+#     if status == 'cms':
+#         height = st.number_input('Centimeters')
+#         try:
+#             bmi = weight / ((height / 100) ** 2)
+#         except:
+#             st.text("Enter some value of height")
 
-    elif status == 'meters':
-        height = st.number_input('Meters')
-        try:
-            bmi = weight / (height ** 2)
-        except:
-            st.text("Enter some value of height")
+#     elif status == 'meters':
+#         height = st.number_input('Meters')
+#         try:
+#             bmi = weight / (height ** 2)
+#         except:
+#             st.text("Enter some value of height")
 
-    else:
-        height = st.number_input('Feet')
-        try:
-            bmi = weight / (((height / 3.28)) ** 2)
-        except:
-            st.text("Enter some value of height")
+#     else:
+#         height = st.number_input('Feet')
+#         try:
+#             bmi = weight / (((height / 3.28)) ** 2)
+#         except:
+#             st.text("Enter some value of height")
 
-    if st.button('calculate BMI'):
-        st.text("Your BMI Index is {}.".format(bmi))
+#     if st.button('calculate BMI'):
+#         st.text("Your BMI Index is {}.".format(bmi))
 
-        if (bmi < 16):
-            st.error("You are Extremely Underweight")
-        elif (bmi >= 16 and bmi < 18.5):
-            st.warning("You are Underweight")
-        elif (bmi >= 18.5 and bmi < 25):
-            st.success("Healthy")
-        elif (bmi >= 25 and bmi < 30):
-            st.warning("Overweight")
-        elif (bmi >= 30):
-            st.error("Extremely Overweight")
+#         if (bmi < 16):
+#             st.error("You are Extremely Underweight")
+#         elif (bmi >= 16 and bmi < 18.5):
+#             st.warning("You are Underweight")
+#         elif (bmi >= 18.5 and bmi < 25):
+#             st.success("Healthy")
+#         elif (bmi >= 25 and bmi < 30):
+#             st.warning("Overweight")
+#         elif (bmi >= 30):
+#             st.error("Extremely Overweight")
 
 
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-st.title('Traffic sign')
+st.title('Traffic sign decection')
 st.subheader("from the mind of Team ZAC.")
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'About'],
+    selected = option_menu("Main Menu", ["Demo", ' More info'],
         icons=['house', 'info-circle'], menu_icon="cast", default_index=1)
     selected
 # horizontal Menu
