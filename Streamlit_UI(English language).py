014f35ab2e430244e4bc46d93db9d0aa552fecdb
Waii <76773473+Zinwaiyan274@users.noreply.github.com>
2022-04-26 15:30:41 +0630

    Create Streamlit_UI(English language).py

---
+++ Streamlit_UI(English language).py
@@ -0,0 +1,39 @@
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+def mm():
+    st.title("ကျေးဇူးပြု၍ မြန်မာဘာသာစကားအတွက် ဤနေရာကိုနိုပ်ပါ။")
+    st.subheader("https://share.streamlit.io/zinwaiyan274/streamlit_testing/main/main.py")
+
+
+def main():
+    st.title("Traffic sign detection")
+    st.subheader("from the mind of Team ZAC.")
+
+    with st.sidebar:
+        selected = option_menu("Main Menu", ["Demo", 'Info'],
+                               icons=['house', 'info-circle'], menu_icon="cast", default_index=1)
+
+        st.subheader("💬Language")
+        if selected == "Demo" or "Info":
+            selected0 = option_menu(None, ["English", "မြန်မာ"],
+                                    icons=['fgngf', "translate"], orientation="horizontal")
+            st.text("©2022_Team_ZAC")
+    if selected0 == "မြန်မာ":
+        mm()
+
+
+    if selected == "Info":
+        selected1 = option_menu(None, ["Term and con", "Developer contact", "blabla", ],
+                            icons=['clipboard', 'chat-right-dots', "flag fill"],
+                            menu_icon="cast", default_index=0, orientation="horizontal")
+
+    if selected == "Demo":
+       selected0 = option_menu(None, ["Demonstration", "Computer Vision", " Evaluation Metric", ],
+                            icons=['activity', 'eye-fill', "check2-circle"],
+                            menu_icon="cast", default_index=0, orientation="horizontal")
+
+main()
+
