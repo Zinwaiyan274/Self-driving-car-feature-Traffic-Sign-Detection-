75e4d5dc52379660e15d106cc085ce0bf6c10137
Waii <76773473+Zinwaiyan274@users.noreply.github.com>
2022-04-30 09:27:47 +0630

    Update English.py

--- English.py
+++ English.py
@@ -14,7 +14,7 @@ import streamlit as st
 
 
 def demo():
-    st.markdown("Try")
+    st.markdown("Predit your sign")
     uploaded_file = st.file_uploader("Choose a file")
 
     if uploaded_file is not None:
